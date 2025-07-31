b91af3e14ff8bf7879e0ee7ab1a7a6590926fa34
LeTomah <thomas.richard@etu.minesparis.psl.eu>
2025-07-31 10:05:10 +0200

    Create Network_test.py

---
+++ Code/Network_test.py
@@ -0,0 +1,47 @@
+import pandapower as pp
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Crée le réseau (reprend ton code ici)
+net = pp.create_empty_network()
+
+# Bases
+Pbase = 100  # MVA
+Vbase_high = 110  # kV
+Vbase_medium = 20  # kV
+Vbase_low = 0.4  # kV
+
+# Buses
+b0 = pp.create_bus(net, vn_kv=Vbase_high, name="Bus grid", geodata=(0,10))
+b1 = pp.create_bus(net, vn_kv=Vbase_high, name="DSO 1", geodata=(5,12))
+b2 = pp.create_bus(net, vn_kv=Vbase_high, name="DSO 2", geodata=(5,8))
+b3 = pp.create_bus(net, vn_kv=Vbase_medium, name="Bus MV_1", geodata=(10,12))
+b4 = pp.create_bus(net, vn_kv=Vbase_medium, name="Bus MV_2", geodata=(10,8))
+b5 = pp.create_bus(net, vn_kv=Vbase_low, name="Bus LV_1", geodata=(15,12))
+b6 = pp.create_bus(net, vn_kv=Vbase_low, name="Bus LV_2", geodata=(15,8))
+b7 = pp.create_bus(net, vn_kv=Vbase_medium, name="Bus éolien", geodata=(10,14))
+b8 = pp.create_bus(net, vn_kv=Vbase_medium, name="Bus industriel", geodata=(10,10))
+b9 = pp.create_bus(net, vn_kv=Vbase_low, name="Bus load", geodata=(15,14))
+b10 = pp.create_bus(net, vn_kv=Vbase_low, name="Bus PV", geodata=(15,10))
+
+# Lignes
+pp.create_line(net, b0, b1, length_km=12, std_type='149-AL1/24-ST1A 110.0', name="line1")
+pp.create_line(net, b1, b2, length_km=10, std_type='149-AL1/24-ST1A 110.0', name="line2")
+pp.create_line(net, b0, b2, length_km=10, std_type='149-AL1/24-ST1A 110.0', name="line3")
+pp.create_line(net, b3, b7, length_km=0.23, std_type='NA2XS2Y 1x185 RM/25 12/20 kV', name="line4")
+pp.create_line(net, b4, b8, length_km=0.3, std_type='NA2XS2Y 1x185 RM/25 12/20 kV', name="line5")
+pp.create_line(net, b5, b9, length_km=3, std_type='94-AL1/15-ST1A 0.4', name="line6")
+pp.create_line(net, b6, b10, length_km=0.45, std_type='94-AL1/15-ST1A 0.4', name="line7")
+
+# Transformateurs
+pp.create_transformer(net, b1, b3, std_type="25 MVA 110/20 kV", name="TR_1")
+pp.create_transformer(net, b2, b4, std_type="25 MVA 110/20 kV", name="TR_2")
+pp.create_transformer(net, b3, b5, std_type="0.4 MVA 20/0.4 kV", name="TR_3")
+pp.create_transformer(net, b4, b6, std_type="0.4 MVA 20/0.4 kV", name="TR_4")
+
+# Éléments de bus
+pp.create_ext_grid(net, bus=b0, vm_pu=1.00, name="Grid Connection")
+pp.create_load(net, bus=b9, p_mw=0.02, q_mvar=0.02, name="Residential Load")
+pp.create_gen(net, bus=b10, p_mw=0.2, vm_pu=1.00, name="PV", max_q_mvar=200, min_q_mvar=0, max_p_mw=300, min_p_mw=0, controllable=False)
+pp.create_gen(net, bus=b7, p_mw=6.5, vm_pu=1.0, name="Wind Turbine", controllable=False)
+pp.create_load(net, bus=b8, p_mw=6, q_mvar=1.8, name="Industry")
