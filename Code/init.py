81ea88be6f390b1ef47a6c0679b855d1810b7702
LeTomah <thomas.richard@etu.minesparis.psl.eu>
2025-08-11 10:35:39 +0200

    Adding init.py

---
+++ Code/init.py
@@ -0,0 +1,26 @@
+"""
+Point d’entrée unique du projet.
+Il suffit de choisir :
+    • le fichier IEEE (.txt)
+    • les nœuds opérationnels
+    • les parents
+    • les enfants
+puis de lancer :  init.py
+"""
+from check_requirements import install_missing_packages
+from optimization import optim_problem
+
+# ---- Paramétrage utilisateur ----
+TEST_CASE = "test_network.py"           # Chemin de votre fichier IEEE
+OPERATIONAL_NODES = [0,1,2,11,12]    # Nœuds conservés dans le sous-réseau
+PARENT_NODES      = [0]             # Injectent la puissance (P_plus)
+CHILDREN_NODES    = [1,2]       # Absorbent la puissance (P_minus)
+# ---------------------------------
+
+install_missing_packages()      # requirements.txt auto-install
+optim_problem(
+    test_case=TEST_CASE,
+    operational_nodes=OPERATIONAL_NODES,
+    parent_nodes=PARENT_NODES,
+    children_nodes=CHILDREN_NODES
+)
