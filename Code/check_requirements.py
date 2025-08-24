0633b40cb1eadb55ab8791cc6e9a7f84aae3326e
LeTomah <thomas.richard@etu.minesparis.psl.eu>
2025-08-24 18:28:52 +0200

    refactor: update data paths

--- Code/check_requirements.py
+++ Code/check_requirements.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 
-def install_missing_packages(requirements_file='requirements.txt'):
+
+def install_missing_packages(requirements_file: str = "Data/requirements.txt") -> None:
     import importlib.util
     import subprocess
     import sys
 
-    # Construire le chemin absolu vers requirements.txt
-    requirements_path = Path(__file__).parent.parent / "requirements.txt"
+    requirements_path = Path(__file__).parent / requirements_file
 
-    with open(requirements_path, 'r') as file:
-        packages = [line.strip() for line in file if line.strip() and not line.startswith('#')]
+    with open(requirements_path, "r") as file:
+        packages = [line.strip() for line in file if line.strip() and not line.startswith("#")]
 
     for package in packages:
         if importlib.util.find_spec(package) is None:
@@ -18,5 +18,6 @@ def install_missing_packages(requirements_file='requirements.txt'):
         else:
             print(f"{package} est déjà installé.")
 
+
 if __name__ == "__main__":
-    install_missing_packages("requirements.txt")
+    install_missing_packages()
